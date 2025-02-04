7cfd3f5b6a7cc9e3d4c65858648470388ee329b2
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-02-04 14:39:38 +0000

    Cleaned up most code and fixed comments.

--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 /// <summary>
-/// A class to control the top down character.
+/// A class to control the top-down character.
 /// Implements the player controls for moving and shooting.
 /// Updates the player animator so the character animates based on input.
 /// </summary>
@@ -11,30 +11,30 @@ public class TopDownCharacterController : MonoBehaviour
     #region Framework Variables
     
     [Header("Movement parameters")]
-    // Reference for the projectile object
+    // Reference for the projectile object.
     [SerializeField] GameObject m_projectilePrefab;
-    // The point at which the projectile spawns
+    // The point at which the projectile spawns.
     [SerializeField] private Transform m_firePoint;
-    // The speed of the projectile
+    // The speed of the projectile.
     [SerializeField] float m_projectileSpeed;
-    //The inputs that we need to retrieve from the input system.
+    // The inputs that we need to retrieve from the input system.
     private InputAction m_moveAction;
     private InputAction m_attackAction;
 
-    //The components that we need to edit to make the player move smoothly.
+    // The components that we need to edit to make the player move smoothly.
     private Animator m_animator;
     private Rigidbody2D m_rigidbody;
     
-    //The direction that the player is moving in.
+    // The direction that the player is moving in.
     private Vector2 m_playerDirection;
     
-    //The speed at which the player moves
+    // The speed at which the player moves.
     [SerializeField] private float m_playerSpeed = 200f;
     //The maximum speed the player can move
     [SerializeField] private float m_playerMaxSpeed = 1000f;
     
-    // The speed at which the player rotates
-    [SerializeField] private float m_playerRotationSpeed = 40f;
+    // The speed at which the player rotates.
+    [SerializeField] private float m_playerRotationSpeed = 10f;
     #endregion
 
     /// <summary>
@@ -44,28 +44,28 @@ public class TopDownCharacterController : MonoBehaviour
     
     private void Awake()
     {
-        //bind movement inputs to variables
+        // Bind movement inputs to variables.
         m_moveAction = InputSystem.actions.FindAction("Move");
         m_attackAction = InputSystem.actions.FindAction("Attack");
         
-        //get components from Character game object so that we can use them later.
+        // Get components from Character game object so that we can use them later.
         m_animator = GetComponent<Animator>();
         m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     /// <summary>
-    /// When a fixed update loop is called, it runs at a constant rate, regardless of pc performance.
+    /// When a fixed update loop is called, it runs at a constant rate, regardless of PC performance.
     /// This ensures that physics are calculated properly.
     /// </summary>
     private void FixedUpdate()
     {
-        //clamp the speed to the maximum speed for if the speed has been changed in code.
+        // Clamp the speed to the maximum speed for if the speed has been changed in code.
         float speed = m_playerSpeed > m_playerMaxSpeed ? m_playerMaxSpeed : m_playerSpeed;
         
         // Applies a force to make the player move in the direction they are facing if W or the up arrow is pressed.
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            GetComponent<Rigidbody2D>().AddForce(transform.up * (Time.deltaTime * speed * Input.GetAxis("Vertical")));
+            m_rigidbody.AddForce(transform.up * (Time.deltaTime * speed * Input.GetAxis("Vertical")));
         }
     }
     
@@ -76,17 +76,16 @@ public class TopDownCharacterController : MonoBehaviour
     /// </summary>
     void Update()
     {
-        // store any movement inputs into m_playerDirection - this will be used in FixedUpdate to move the player.
+        // Store any movement inputs into m_playerDirection - this will be used in FixedUpdate to move the player.
         m_playerDirection = m_moveAction.ReadValue<Vector2>();
 
-        // check if an attack has been triggered.
+        // Check if an attack has been triggered.
         if (m_attackAction.IsPressed())
         {
-            // just log that an attack has been registered for now
-            // we will look at how to do this in future sessions.
             Fire();
         }
 
+        // Rotates the player if the correct keys are pressed.
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate (0, 0, m_playerRotationSpeed * Time.fixedDeltaTime);
@@ -102,6 +101,7 @@ public class TopDownCharacterController : MonoBehaviour
     {
         GameObject projectileToSpawn = Instantiate(m_projectilePrefab, m_firePoint.position, Quaternion.identity);
 
+        // Spawns projectile.
         if (projectileToSpawn.GetComponent<Rigidbody2D>())
         {
             projectileToSpawn.GetComponent<Rigidbody2D>().linearVelocity = m_playerDirection * m_projectileSpeed;
