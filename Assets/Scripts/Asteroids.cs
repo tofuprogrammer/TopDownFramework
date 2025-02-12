fde390abea879241159056217fd5465cae4b709c
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-02-12 14:37:01 +0000

    Updating asteroid-spawning-system branch (#7)
    * Started work on asteroid spawning system
    
    Asteroids spawn correctly however position and frequency needs to be changed for the game. Currently, the game spawns between three and six random asteroids on the same position every frame.
    
    * Final session commit
    
    * Merging asset DB changes
    
    * Pulling in old branches for new branch structure (#6)
    
    * Started work on asteroid spawning system
    
    Asteroids spawn correctly however position and frequency needs to be changed for the game. Currently, the game spawns between three and six random asteroids on the same position every frame.
    
    * Final session commit
    
    * Merging asset DB changes
    
    * Preparing for merge

--- Assets/Scripts/Asteroids.cs
+++ Assets/Scripts/Asteroids.cs
@@ -10,29 +10,21 @@ public class Asteroids : MonoBehaviour
     private int numberOfAsteroids;
     #endregion
 
-    void Start()
-    {
-        Invoke(nameof(SpawnAsteroid), Random.Range(2.0f, 5.0f));
-    }
-    
     void Update()
     {
-        
-        
+        numberOfAsteroids = Random.Range(3, 6);
+        asteroidToSpawn = Random.Range(1, asteroidPrefabs.Length + 1);
+        SpawnAsteroid();
     }
     
     void SpawnAsteroid()
     {
-        numberOfAsteroids = Random.Range(3, 6);
         for (int i = 0; i < numberOfAsteroids; i++)
         {
-            asteroidToSpawn = Random.Range(1, asteroidPrefabs.Length + 1);
-            
             if (asteroidToSpawn > 0 && asteroidToSpawn <= asteroidPrefabs.Length)
             {
                 Instantiate(asteroidPrefabs[asteroidToSpawn - 1], transform.position, Quaternion.identity);
             }
         }
-        Invoke(nameof(SpawnAsteroid), Random.Range(2.0f, 5.0f));
     }
 }
